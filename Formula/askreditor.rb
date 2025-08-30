0ce1722d33089254c0aac0bd49ea0c075bc2da9f
sakitibi <sakitibi10nin@icloud.com>
2025-08-30 11:20:18 +0900

    Fixed askrediter.rb

--- Formula/askreditor.rb
+++ Formula/askreditor.rb
@@ -1,23 +1,23 @@
 class Askreditor < Formula
   desc "CLI editor for .askr files with Supabase replace integration"
   homepage "https://github.com/sakitibi/askreditor"
-  version "2.0.20"
+  version "2.0.30"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.20/askreditor-2.0.20-darwin-arm64.tar.gz"
-      sha256 "d153b2314b0e1c57821c126823faf52aaaf4b9e167d77cd1cc86d46d72061525"
+      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.30/askreditor-2.0.30-darwin-arm64.tar.gz"
+      sha256 "751fce21b693ee8766dd44c6526b5cf7eaa0a875fdd0dcd57e699054de246ce7"
     else
-      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.20/askreditor-2.0.20-darwin-amd64.tar.gz"
-      sha256 "460ef771d389bf0a96181cd78e8584857e002ed115f5637210336c704859ac6e"
+      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.30/askreditor-2.0.30-darwin-amd64.tar.gz"
+      sha256 "9f15250de550fdcbf432008305d02c482651f0fc9a556749daf530c138d7df32"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.20/askreditor-2.0.20-linux-arm64.tar.gz"
-      sha256 "bd668b6702e87aa87f00fcf59f7ce1934ca8160e47622d52532c6b911564579f"
+      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.30/askreditor-2.0.30-linux-arm64.tar.gz"
+      sha256 "ebfb7203dfd46a8fc23cf43bb5062458dd920476f5d990cb8deacf05a5afdd9d"
     else
-      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.20/askreditor-2.0.20-linux-amd64.tar.gz"
-      sha256 "a0859e87c2dd0daf79437acf36f53968aa0c203ba8c3e71b18eb55ff83769a2b"
+      url "https://github.com/sakitibi/askreditor/releases/download/v2.0.30/askreditor-2.0.30-linux-amd64.tar.gz"
+      sha256 "3413679582e501e05f94a300139cb658dac4be96eb8ed8d8571117f495a419fd"
     end
   end
 
